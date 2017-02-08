a9b58daca32d6d7b458adce4d868a1466764cfbf
GA-MO <thawatchai.deartk@gmail.com>
2017-02-09 02:28:33 +0700

    Update dependencies

--- server/server.js
+++ server/server.js
@@ -3,19 +3,15 @@ import express from 'express'
 import ssr from './ssr'
 import config from '../configs'
 
-const PORT = config.port;
 const app = express()
-// const targetUrl = `http://${config.host}:${config.apiPort}`
-// const proxy = httpProxy.createProxyServer({
-//   target: targetUrl
-// })
-
-// app.use('/api', (req, res) => {
-//   proxy.web(req, res, { target: `${targetUrl}/api` });
-// })
 
 app.use('/static', express.static('static'));
 app.use(ssr)
+
+
+app.set('port', (process.env.PORT || config.port));
+const PORT = app.get('port');
+
 
 app.listen(PORT, error => {
   if (error) {
