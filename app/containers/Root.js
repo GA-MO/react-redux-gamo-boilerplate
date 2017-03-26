b4f32bffa331e238c325c5f7248ebe557735c268
GA-MO <thawatchai.deartk@gmail.com>
2017-03-26 17:46:34 +0700

    Clean code

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -2,24 +2,18 @@ import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import { Router, browserHistory, useRouterHistory } from 'react-router'
 import { createHashHistory } from 'history'
-
 import configureStore from '../store/configureStore'
 import routes from '../routes'
 
-export default class Root extends Component {
-  render() {
-    const initialState = window.__INITIAL_STATE__;
-    let history;
-    if (process.env.BUILD_ENV === 'client') {
-      history = useRouterHistory(createHashHistory)({ queryKey: false });
-    } else {
-      history = browserHistory;
-    }
-    const store = configureStore(initialState, history);
-    return (
-      <Provider store={store} key="provider">
-        <Router history={history} routes={routes} />
-      </Provider>
-    )
-  }
-}
+export default () => {
+  /* Get initialstate */
+  const initialState = window.__INITIAL_STATE__;
+  const isBuildClient = process.env.BUILD_ENV === 'client';
+  const history = isBuildClient ? useRouterHistory(createHashHistory)({ queryKey: false }) : browserHistory;
+  const store = configureStore(initialState, history);
+  return (
+    <Provider store={store} key="provider">
+      <Router history={history} routes={routes} />
+    </Provider>
+  );
+};
