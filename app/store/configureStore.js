bf91c0e5d5d391ffc5d62eb0baa4a1708e150a90
GA-MO <thawatchai.deartk@gmail.com>
2017-05-05 18:18:51 +0700

    Update config redux logger

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { routerMiddleware } from 'react-router-redux'
 // import thunk from 'redux-thunk';
-import createLogger from 'redux-logger'
+import { createLogger } from 'redux-logger'
 import rootReducer from '../reducers'
 import middleware from '../actions/middleware';
 
