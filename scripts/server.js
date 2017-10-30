1d0059e0b7336de9ec29930c4f69ac3b8406f1d8
GA-MO <thawatchai.deartk@gmail.com>
2017-10-30 16:46:36 +0700

    move dependecies

--- scripts/server.js
+++ scripts/server.js
@@ -1,18 +1,18 @@
-import express from 'express'
-import ssr from './ssr'
-import config from '../configs'
+// import express from 'express'
+// import ssr from './ssr'
+// import config from '../configs'
 
-const app = express()
+// const app = express()
 
-app.use(express.static('build'))
-app.use(ssr)
-app.set('port', process.env.PORT || config.port)
-const PORT = app.get('port')
+// app.use(express.static('build'))
+// app.use(ssr)
+// app.set('port', process.env.PORT || config.port)
+// const PORT = app.get('port')
 
-app.listen(PORT, error => {
-  if (error) {
-    console.error(error)
-  } else {
-    console.info(`==> Listening on port ${PORT}.`)
-  }
-})
+// app.listen(PORT, error => {
+//   if (error) {
+//     console.error(error)
+//   } else {
+//     console.info(`==> Listening on port ${PORT}.`)
+//   }
+// })
